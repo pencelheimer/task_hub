c0216021f493ef76fc7586f05e1496f1796831da
pencelheimer <pencelheimer@proton.me>
2025-06-06 21:38:00 +0300

    fix(auth): fixed oauth2

--- src/controllers/oauth2.rs
+++ src/controllers/oauth2.rs
@@ -1,34 +1,42 @@
-use crate::models::{o_auth2_sessions, users, users::OAuth2UserProfile};
-use axum::Extension;
-use axum_session::Session;
-use axum_session::SessionNullPool;
-use loco_oauth2::controllers::oauth2::get_authorization_url;
-use loco_oauth2::OAuth2ClientStore;
-
-use loco_openapi::prelude::*;
+use axum::{extract::Query, Extension};
+use axum_session::{SameSite, Session, SessionNullPool};
+use loco_oauth2::{
+    controllers::{
+        middleware::OAuth2CookieUser,
+        oauth2::{callback_jwt, google_callback_cookie, AuthParams},
+    },
+    OAuth2ClientStore,
+};
 use loco_rs::prelude::*;
+use tower_cookies::{Cookie, Cookies};
 
-use loco_oauth2::controllers::oauth2::google_callback_jwt;
+use crate::{
+    common::settings::Settings,
+    models::{
+        o_auth2_sessions,
+        users::{self, OAuth2UserProfile},
+    },
+};
 
-/// Redirect to OAuth2
-///
-/// Redirect handler to the authorization URL for the `OAuth2` flow
-/// This will redirect the user to the `OAuth2` provider's login page
-/// and then to the callback URL
-#[utoipa::path(
-    get,
-    path = "/api/oauth2/google",
-    tag = "oauth2",
-    responses(
-        (status = 307, description = "Redirect to OAuth2 URL"),
-        (status = 500, description = "Internal server error")
-    ),
-)]
-pub async fn redirect_to_google_authorization_url(
+async fn protected(
+    State(ctx): State<AppContext>,
+    // Extract the user from the Cookie via middleware
+    user: OAuth2CookieUser<OAuth2UserProfile, users::Model, o_auth2_sessions::Model>,
+) -> Result<Response> {
+    let user: &users::Model = user.as_ref();
+    let jwt_secret = ctx.config.get_jwt_config()?;
+    // Generate a JWT token
+    let token = user
+        .generate_jwt(&jwt_secret.secret, jwt_secret.expiration)
+        .or_else(|_| unauthorized("unauthorized!"))?;
+    // Return the user and the token in JSON format
+    format::json(user)
+}
+
+pub async fn google_authorization_url(
     session: Session<SessionNullPool>,
     Extension(oauth2_store): Extension<OAuth2ClientStore>,
-) -> Result<impl IntoResponse> {
-    // Get the `google` Authorization Code Grant client from the `OAuth2ClientStore`
+) -> Result<String> {
     let mut client = oauth2_store
         .get_authorization_code_client("google")
         .await
@@ -36,30 +44,79 @@ pub async fn redirect_to_google_authorization_url(
             tracing::error!("Error getting client: {:?}", e);
             Error::InternalServerError
         })?;
-    // Get the authorization URL and save the csrf token in the session
-    let auth_url = get_authorization_url(session, &mut client).await;
+
+    let (auth_url, csrf_token) = client.get_authorization_url();
+    session.set("CSRF_TOKEN", csrf_token.secret().to_owned());
+
+    session.update();
+
     drop(client);
-    Ok(axum::response::Redirect::temporary(&auth_url).into_response())
+
+    Ok(auth_url.to_string())
+}
+
+pub async fn google_callback_jwt(
+    State(ctx): State<AppContext>,
+    session: Session<SessionNullPool>,
+    cookies: Cookies,
+    Query(params): Query<AuthParams>,
+    Extension(oauth2_store): Extension<OAuth2ClientStore>,
+) -> Result<impl IntoResponse> {
+    let settings = &Settings::from_opt_json(&ctx.config.settings)?;
+
+    let mut client = oauth2_store
+        .get_authorization_code_client("google")
+        .await
+        .map_err(|e| {
+            tracing::error!("Error getting client: {:?}", e);
+            Error::InternalServerError
+        })?;
+
+    let jwt_secret = ctx.config.get_jwt_config()?;
+
+    let user = callback_jwt::<
+        OAuth2UserProfile,
+        users::Model,
+        o_auth2_sessions::Model,
+        SessionNullPool,
+    >(&ctx, session, params, &mut client)
+    .await?;
+
+    drop(client);
+
+    let token = user
+        .generate_jwt(&jwt_secret.secret, jwt_secret.expiration)
+        .or_else(|_| unauthorized("unauthorized!"))?;
+
+    let cookie = Cookie::build(("auth_token", token.clone()))
+        .path("/")
+        .domain(settings.backend.to_owned())
+        .http_only(true)
+        .secure(true)
+        .same_site(SameSite::None)
+        .max_age(time::Duration::seconds(jwt_secret.expiration as i64))
+        .build();
+
+    cookies.add(cookie);
+
+    format::redirect(format!("https://{}/profile", settings.frontend).as_str())
 }
 
 pub fn routes() -> Routes {
     Routes::new()
-        .prefix("api/oauth2/google")
+        .prefix("api/oauth2")
+        .add("/google", get(google_authorization_url))
+        // Route for the Cookie callback
         .add(
-            "/",
-            openapi(
-                get(redirect_to_google_authorization_url),
-                routes!(redirect_to_google_authorization_url),
-            ),
-        )
-        // Route for the JWT callback
-        .add(
-            "/callback/jwt",
-            get(google_callback_jwt::<
+            "/google/callback/cookie",
+            get(google_callback_cookie::<
                 OAuth2UserProfile,
                 users::Model,
                 o_auth2_sessions::Model,
                 SessionNullPool,
             >),
         )
+        // Route for the JWT callback
+        .add("/google/callback/jwt", get(google_callback_jwt))
+        .add("/protected", get(protected))
 }
--- src/initializers/axum_session.rs
+++ src/initializers/axum_session.rs
@@ -1,6 +1,10 @@
 use async_trait::async_trait;
 use axum::Router as AxumRouter;
+use axum_session::SessionConfig;
 use loco_rs::prelude::*;
+use tower_cookies::cookie::SameSite;
+
+use crate::common::settings::Settings;
 
 pub struct AxumSessionInitializer;
 
@@ -10,9 +14,14 @@ impl Initializer for AxumSessionInitializer {
         "axum-session".to_string()
     }
 
-    async fn after_routes(&self, router: AxumRouter, _ctx: &AppContext) -> Result<AxumRouter> {
-        let session_config =
-            axum_session::SessionConfig::default().with_table_name("sessions_table");
+    async fn after_routes(&self, router: AxumRouter, ctx: &AppContext) -> Result<AxumRouter> {
+        let settings = &Settings::from_opt_json(&ctx.config.settings)?;
+
+        let session_config = SessionConfig::default()
+            .with_table_name("sessions_table")
+            .with_cookie_domain(settings.backend.to_owned())
+            .with_secure(true)
+            .with_cookie_same_site(SameSite::None);
 
         let session_store =
             axum_session::SessionStore::<axum_session::SessionNullPool>::new(None, session_config)
